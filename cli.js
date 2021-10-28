78b90221d438279f4f0afc267bc73e0c4a3fad24
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-10-28 16:29:55 +0100

    this cli is cool now

--- cli.js
+++ cli.js
@@ -1,21 +1,39 @@
 const Thermostat = require("./thermostat");
-const thermostat = new Thermostat;
-const readlineSync = require('readline-sync')
+const thermostat = new Thermostat();
+const readlineSync = require("readline-sync");
 
-
+const maxReached = () => {
+  if (thermostat.powerSavingMode == true && thermostat.temperature == 25) {
+    return "(maximum reached)";
+  } else if (
+    thermostat.powerSavingMode == false &&
+    thermostat.temperature == 32
+  ) {
+    return "(maximum reached)";
+  } else {
+    return "";
+  }
+};
 
 do {
-  console.log(`Temperature is ${thermostat.getTemperature()}`)
-  command = readlineSync.question('Enter command ')
-  switch(command) {
-    case 'up':
-      thermostat.up()
+  console.log(`Temperature is ${thermostat.getTemperature()} ${maxReached()}`);
+  command = readlineSync.question("Enter command ");
+  switch (command) {
+    case "up":
+      thermostat.up();
       break;
-    case 'down':
-      thermostat.down()
+    case "down":
+      thermostat.down();
+      break;
+    case "psm off":
+      thermostat.setPowerSavingMode(false);
+      console.log("PSM is off");
+      break;
+    case "psm on":
+      thermostat.setPowerSavingMode(true);
+      console.log("PSM is on");
       break;
     default:
       break;
-  } 
-} while(true)
-
+  }
+} while (true);
