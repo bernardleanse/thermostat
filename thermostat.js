f0eee287796a23cfd4227199a40d80e4dfb4429b
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-10-28 15:21:35 +0100

    f

--- thermostat.js
+++ thermostat.js
@@ -1,30 +1,41 @@
 class Thermostat {
-  constructor(){
-    this.temperature = 20
-    this.powerSavingMode = false
+  constructor() {
+    this.temperature = 20;
+    this.powerSavingMode = true;
   }
   getTemperature() {
-    return this.temperature
+    return this.temperature;
   }
   up() {
-    if(this.powerSavingMode = true && this.temperature < 25){
-      this.temperature += 1
+    if (
+      (this.powerSavingMode == true && this.temperature < 25) ||
+      (this.powerSavingMode == false && this.temperature < 32)
+    ) {
+      this.temperature += 1;
     }
-    else if(this.powerSavingMode = false) {
-      this.temperature += 1
-    }   
   }
   down() {
-    this.temperature -= 1
+    this.temperature -= 1;
   }
   setPowerSavingMode(bool) {
-    if(bool == true){
-      this.powerSavingMode = true
-    }
-    else if(bool == false) {
-      this.powerSavingMode = false
+    if (bool == true) {
+      this.powerSavingMode = true;
+    } else if (bool == false) {
+      this.powerSavingMode = false;
     }
   }
-}   
+  reset() {
+    this.temperature = 20;
+  }
+  getEnergyUsage() {
+    if (this.temperature < 18) {
+      return "Low-usage";
+    } else if (this.temperature <= 25) {
+      return "Medium-usage";
+    } else {
+      return "High-usage";
+    }
+  }
+}
 
-module.exports = Thermostat
+module.exports = Thermostat;
--- thermostat.test.js
+++ thermostat.test.js
@@ -1,37 +1,64 @@
-const Thermostat = require('./thermostat')
+const Thermostat = require("./thermostat");
 
 describe(Thermostat, () => {
   beforeEach(() => {
     this.thermostat = new Thermostat();
   });
-  describe('#getTemperature', () => {
-    it('gets the temperature', () => {
+  describe("#getTemperature", () => {
+    it("gets the temperature", () => {
       expect(this.thermostat.getTemperature()).toBe(20);
     });
   });
-  describe('#up', () => {
-    it('raises the temperature', () => {
-      this.thermostat.up()
-      this.thermostat.up()
+  describe("#up", () => {
+    it("raises the temperature", () => {
+      this.thermostat.up();
+      this.thermostat.up();
       expect(this.thermostat.getTemperature()).toBe(22);
     });
   });
-  describe('#down', () => {
-    it('lowers the temperature', () => {
-      this.thermostat.down()
-      expect(this.thermostat.getTemperature()).toBe(19)
+  describe("#down", () => {
+    it("lowers the temperature", () => {
+      this.thermostat.down();
+      expect(this.thermostat.getTemperature()).toBe(19);
     });
   });
-  describe('#setPowerSavingMode', () => {
-    it('when true temperature is capped at 25', () => {
-      for(i = 0; i < 10; i++) {
-        this.thermostat.up()
+  describe("#setPowerSavingMode", () => {
+    it("when true temperature is capped at 25", () => {
+      for (i = 0; i < 10; i++) {
+        this.thermostat.up();
       }
-      this.thermostat.setPowerSavingMode(true)
-      expect(this.thermostat.getTemperature()).toBe(25)
+      expect(this.thermostat.getTemperature()).toBe(25);
+    });
+    it("when false temperature is capped at 32", () => {
+      this.thermostat.setPowerSavingMode(false);
+      for (i = 0; i < 40; i++) {
+        this.thermostat.up();
+      }
+      expect(this.thermostat.getTemperature()).toBe(32);
     });
   });
-  xdescribe('', () => {
-    it('', () => {});
+  describe("#reset", () => {
+    it("resets the temperature", () => {
+      this.thermostat.up();
+      this.thermostat.reset();
+      expect(this.thermostat.getTemperature()).toBe(20);
+    });
+  });
+  describe("#getEnergyUsage", () => {
+    it("returns low-usage when temperature is below 18", () => {
+      this.thermostat.temperature = 17;
+      expect(this.thermostat.getEnergyUsage()).toBe("Low-usage");
+    });
+    it("returns medium-usage if temperature is between 18 and 25", () => {
+      this.thermostat.temperature = 22;
+      expect(this.thermostat.getEnergyUsage()).toBe("Medium-usage");
+    });
+    it("returns high-usage if temperature is above 25", () => {
+      this.thermostat.temperature = 28;
+      expect(this.thermostat.getEnergyUsage()).toBe("High-usage");
+    });
+  });
+  xdescribe("", () => {
+    it("", () => {});
   });
 });
