563d7a885096745d4549e6c711fdf4b5307b8e1b
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-28 12:16:50 +0100

    set up test

---
+++ thermostat.test.js
@@ -0,0 +1,16 @@
+const Thermostat = require('./thermostat')
+
+describe(Thermostat, () => {
+  beforeEach(() => {
+    this.thermostat = new Thermostat();
+  });
+  describe('#getTemperature', () => {
+    it('gets the temperature', () => {});
+  });
+  // describe('', () => {
+  //   it('', () => {});
+  // });
+  // describe('', () => {
+  //   it('', () => {});
+  // });
+});
