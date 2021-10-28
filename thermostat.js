8a01c4170d6f25ad1452c92060ac1dac483b2cf7
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-28 14:44:07 +0100

    added methods

--- thermostat.js
+++ thermostat.js
@@ -1,5 +1,30 @@
 class Thermostat {
-
-}
+  constructor(){
+    this.temperature = 20
+    this.powerSavingMode = false
+  }
+  getTemperature() {
+    return this.temperature
+  }
+  up() {
+    if(this.powerSavingMode = true && this.temperature < 25){
+      this.temperature += 1
+    }
+    else if(this.powerSavingMode = false) {
+      this.temperature += 1
+    }   
+  }
+  down() {
+    this.temperature -= 1
+  }
+  setPowerSavingMode(bool) {
+    if(bool == true){
+      this.powerSavingMode = true
+    }
+    else if(bool == false) {
+      this.powerSavingMode = false
+    }
+  }
+}   
 
 module.exports = Thermostat
--- thermostat.test.js
+++ thermostat.test.js
@@ -5,12 +5,33 @@ describe(Thermostat, () => {
     this.thermostat = new Thermostat();
   });
   describe('#getTemperature', () => {
-    it('gets the temperature', () => {});
+    it('gets the temperature', () => {
+      expect(this.thermostat.getTemperature()).toBe(20);
+    });
   });
-  // describe('', () => {
-  //   it('', () => {});
-  // });
-  // describe('', () => {
-  //   it('', () => {});
-  // });
+  describe('#up', () => {
+    it('raises the temperature', () => {
+      this.thermostat.up()
+      this.thermostat.up()
+      expect(this.thermostat.getTemperature()).toBe(22);
+    });
+  });
+  describe('#down', () => {
+    it('lowers the temperature', () => {
+      this.thermostat.down()
+      expect(this.thermostat.getTemperature()).toBe(19)
+    });
+  });
+  describe('#setPowerSavingMode', () => {
+    it('when true temperature is capped at 25', () => {
+      for(i = 0; i < 10; i++) {
+        this.thermostat.up()
+      }
+      this.thermostat.setPowerSavingMode(true)
+      expect(this.thermostat.getTemperature()).toBe(25)
+    });
+  });
+  xdescribe('', () => {
+    it('', () => {});
+  });
 });
